13652f8fabdd57239c43b4af1a0a7720f43067f5
Narangerel Baatardalai <615418@cs.mum.edu>
2023-03-18 15:10:22 -0500

    Nara tested 2

--- ea-project/src/main/java/edu/miu/eaproject/EaProjectApplication.java
+++ ea-project/src/main/java/edu/miu/eaproject/EaProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EaProjectApplication {
-//NARA HAS TESTED
+//NARA HAS TESTED git
 	public static void main(String[] args) {
 		SpringApplication.run(EaProjectApplication.class, args);
 	}
