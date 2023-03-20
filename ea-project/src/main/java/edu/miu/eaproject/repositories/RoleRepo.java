3fdf2530bdcca9469777cfb692dabb69fc8a1176
Abenezer Dana <abenezer.dana@miu.edu>
2023-03-20 14:12:32 -0500

    AuthService added security is inserted pomfile changed

--- ea-project/src/main/java/edu/miu/eaproject/repositories/RoleRepo.java
+++ ea-project/src/main/java/edu/miu/eaproject/repositories/RoleRepo.java
@@ -2,8 +2,6 @@ package edu.miu.eaproject.repositories;
 
 import edu.miu.eaproject.entities.Role;
 import edu.miu.eaproject.entities.enums.RoleType;
-import edu.miu.waa.project.backend.domain.Role;
-import edu.miu.waa.project.backend.enumSet.RoleType;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepo extends CrudRepository<Role,Long> {
