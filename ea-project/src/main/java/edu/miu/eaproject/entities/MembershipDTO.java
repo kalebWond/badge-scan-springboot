58cb890f74c2ac7b0b473fbe9b010b7bc1d5d3f5
Abenezer Dana <abenezer.dana@miu.edu>
2023-03-20 14:47:56 -0500

    add MembershipDTO

--- ea-project/src/main/java/edu/miu/eaproject/entities/MembershipDTO.java
+++ ea-project/src/main/java/edu/miu/eaproject/entities/MembershipDTO.java
@@ -1,7 +1,21 @@
 package edu.miu.eaproject.entities;
 
+import edu.miu.eaproject.entities.enums.MembershipType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class MembershipDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private MembershipType membershipType;
+    private int numberOfAllowances;
+
 }
