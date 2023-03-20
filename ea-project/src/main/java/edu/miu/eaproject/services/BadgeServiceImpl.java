0d31965d1ffb0204948ce5ced2e7369eb95701e3
Narangerel Baatardalai <615418@cs.mum.edu>
2023-03-20 00:24:05 -0500

    CRUD operations for badge is completed

---
+++ ea-project/src/main/java/edu/miu/eaproject/services/BadgeServiceImpl.java
@@ -0,0 +1,51 @@
+package edu.miu.eaproject.services;
+
+import edu.miu.eaproject.entities.Badge;
+import edu.miu.eaproject.entities.enums.BadgeStatus;
+import edu.miu.eaproject.repositories.BadgeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BadgeServiceImpl implements BadgeService {
+
+    @Autowired
+    private BadgeRepository badgeRepository;
+
+    @Override
+    public Badge createBadge() {
+        Badge badge = new Badge();
+        badge.setStatus(BadgeStatus.ACTIVE);
+        return badgeRepository.save(badge);
+    }
+
+    @Override
+    public Badge readBadge(Long id) {
+        Optional<Badge> badge = badgeRepository.findById(id);
+        return badge.isEmpty() ?  null : badge.get();
+    }
+
+    @Override
+    public Badge updateBadge(Badge badge) {
+        badgeRepository.save(badge);
+    }
+
+    @Override
+    public void deleteBadge(Badge badge) {
+        badgeRepository.delete(badge);
+    }
+
+    public void deactivateBadge(Long id) {
+        Badge badge = readBadge(id);
+        badge.setStatus(BadgeStatus.INACTIVE);
+        updateBadge(badge);
+    }
+
+    public void activateBadge(Long id) {
+        Badge badge = readBadge(id);
+        badge.setStatus(BadgeStatus.ACTIVE);
+        updateBadge(badge);
+    }
+}
