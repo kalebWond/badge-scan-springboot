d8e6f77f211ef027f85004fbce2feca6dc6cb643
Kidist Gizachew Zihon <111342@cs.mum.edu>
2023-03-21 10:55:19 -0500

    PlanResponseDTO added and plan crud is done.

--- ea-project/src/main/java/edu/miu/eaproject/services/PlanService.java
+++ ea-project/src/main/java/edu/miu/eaproject/services/PlanService.java
@@ -3,15 +3,16 @@ package edu.miu.eaproject.services;
 import edu.miu.eaproject.entities.Location;
 import edu.miu.eaproject.entities.Plan;
 import edu.miu.eaproject.entities.PlanDTO;
+import edu.miu.eaproject.entities.PlanResponseDTO;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PlanService {
-    PlanDTO createPlan(PlanDTO planDTO);
-    PlanDTO getPlanById(Long planId);
-    List<PlanDTO> getAllPlans();
-    PlanDTO updatePlan(Long planId, PlanDTO planDTO);
+    PlanResponseDTO createPlan(PlanDTO planDTO);
+    PlanResponseDTO getPlanById(Long planId);
+    List<PlanResponseDTO> getAllPlans();
+    PlanResponseDTO updatePlan(Long planId, PlanDTO planDTO);
     void deletePlan(Long planId);
 
 }
